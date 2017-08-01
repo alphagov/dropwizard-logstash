18247e86aafc99a581ceac11443ff910d9923024
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2017-08-01 10:12:03 +0100

    add test for missing Referer header behaviour
    since this adds two separate tests that check the console output, I
    changed the dropwizardapprule from a static ClassRule to an instance
    Rule, so we have a fresh SystemOutRule for each test run.

--- src/test/java/uk/gov/ida/dropwizard/logstash/LogstashConsoleAppenderAppRuleTest.java
+++ src/test/java/uk/gov/ida/dropwizard/logstash/LogstashConsoleAppenderAppRuleTest.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.glassfish.jersey.client.JerseyClientBuilder;
-import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.rules.RuleChain;
@@ -28,10 +28,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LogstashConsoleAppenderAppRuleTest {
 
     public static DropwizardAppRule<TestConfiguration> dropwizardAppRule = new DropwizardAppRule<>(TestApplication.class, ResourceHelpers.resourceFilePath("console-appender-test-application.yml"));
-    public static SystemOutRule systemOutRule = new SystemOutRule().enableLog().muteForSuccessfulTests();
+    public SystemOutRule systemOutRule = new SystemOutRule().enableLog().muteForSuccessfulTests();
 
-    @ClassRule
-    public static TestRule ruleChain = RuleChain
+    @Rule
+    public TestRule ruleChain = RuleChain
             .outerRule(systemOutRule)
             .around(dropwizardAppRule);
 
@@ -68,6 +68,29 @@ public class LogstashConsoleAppenderAppRuleTest {
         // ballpark check that the unit is in the right order of magnitude
         // this test should hopefully catch a value that's erroneously measured in seconds
         assertThat(accessEvent.getElapsedTimeMillis()).isBetween(3,3000);
+    }
+
+    @Test
+    public void testRequestLogWithMissingRefererHeader() throws InterruptedException, IOException {
+        Client client = new JerseyClientBuilder().build();
+
+        final Response response = client.target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/").request()
+                .get();
+
+        assertThat(response.readEntity(String.class)).isEqualTo("hello!");
+
+        // If we try to read systemOutRule too quickly, under some circumstances the appender won't have
+        // successfully written the expected access log line yet.  We haven't got to the bottom of this
+        // but it seems to depend on whether another DropwizardAppRule test has been run before this one.
+        // sleeping for a while fixes the problem
+        Thread.sleep(500);
+
+        final List<AccessEventFormat> list = parseLogsOfType(AccessEventFormat.class);
+
+        List<AccessEventFormat> accessEventStream = list.stream().filter(accessLog -> accessLog.getMethod().equals("GET")).collect(toList());
+        assertThat(accessEventStream.size()).as("check there's an access log in the following:\n%s", systemOutRule.getLog()).isEqualTo(1);
+        AccessEventFormat accessEvent = accessEventStream.get(0);
+        assertThat(accessEvent.getReferer()).isEqualTo("-");
     }
 
     @Test
